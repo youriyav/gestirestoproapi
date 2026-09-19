fb5bc578f4069543c83c4696a93b26011bfdccb5
yavoucko <yavouckolye@gmail.com>
2026-09-19 13:46:23 +0100

    last commit

--- src/database/seeds/main.seeder.ts
+++ src/database/seeds/main.seeder.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm';
 import { runSeeders, Seeder, runSeeder } from 'typeorm-extension';
 import { UserSeeder } from './user.seeder';
-import { MenuSeeder } from './menu.seeder';
+//import { MenuSeeder } from './menu.seeder';
 
 export class MainSeeder implements Seeder {
   constructor(protected seedName: string) {
@@ -9,7 +9,7 @@ export class MainSeeder implements Seeder {
   }
   public async run(dataSource: DataSource): Promise<void> {
     await runSeeders(dataSource, {
-      seeds: [UserSeeder, MenuSeeder],
+      seeds: [UserSeeder],
     });
   }
 }
